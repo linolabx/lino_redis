0a4465e50c54f60636de2c0702b5c7027d1ee812
GeekTR <geektheripper@gmail.com>
2023-11-07 14:17:32 +0800

    fix key join issue

--- utils/key.go
+++ utils/key.go
@@ -8,14 +8,13 @@ func KeyJoin(pathList ...string) string {
 	parts := []string{}
 
 	for _, p := range pathList {
-		parts = append(parts, strings.Trim(p, ":"))
+		if p != "" {
+			parts = append(parts, strings.Trim(p, ":"))
+		}
 	}
 	return strings.Join(parts, ":")
 }
 
 func KeyJoinSlice(path string, pathList ...string) string {
-	if len(pathList) == 0 {
-		return strings.Trim(path, ":")
-	}
-	return strings.Trim(path, ":") + ":" + KeyJoin(pathList...)
+	return KeyJoin(append([]string{path}, pathList...)...)
 }
